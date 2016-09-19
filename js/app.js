3c05b65acb7e3844bd11df3a772d2d61170e5b19
dflatblues <jsanchez.gr@gmail.com>
2016-09-20 00:24:35 +0200

    Working controller

---
+++ js/app.js
@@ -0,0 +1,33 @@
+(function () {
+    'use strict';
+
+    angular.module('LunchCheck', [])
+
+        .controller('LunchCheckController', function ($scope) {
+
+            $scope.checkMenu = function () {
+
+                var message = '';
+
+                if ($scope.menu == null) {
+                    message = 'Please enter data first';
+                }else{
+                    var elements = countElements($scope.menu);
+                    message = (elements > 3) ? 'Too much!' : 'Enjoy!';
+                }
+
+                $scope.message = message;
+            };
+
+            function countElements(menu) {
+
+                // Clean empty strings
+                var menuList = menu.split(',').filter(function(v){return v!=='' && v!==' '});
+
+                return menuList.length;
+            }
+
+        });
+
+
+})();
